1faf77c96eaaecd99484ea424c7fd6917b4cdb61
wuQAQ <302538257@qq.com>
2016-03-11 22:05:59 +0800

    将要写sortList

--- ReversingLinkedList/ReversingLinkedList.cpp
+++ ReversingLinkedList/ReversingLinkedList.cpp
@@ -2,6 +2,74 @@
 
 int main()
 {
+	int firstAddress;
+	int length;
+	int changeList;
 
+	List L;
+	L = (List)malloc(sizeof(struct Node));
+
+	scanf("%d %d %d", &firstAddress, &length, &changeList);
+	L->NextAddress = firstAddress;
+	L->NextPosition = NULL;
+
+	L = CreatLinkedList(firstAddress, length, L);
+	TravalList(L);
+	free(L);
 	return  0;
+}
+
+List CreatLinkedList(int firstAddress, int length, List L)
+{
+	Position PNew;
+	List LTail;
+
+	LTail = L;
+
+	for (int i = 0; i < length; i++)
+	{
+		PNew = (Position)malloc(sizeof(struct Node));
+		scanf("%d %d %d", &PNew->Address, &PNew->Data, &PNew->NextAddress);
+
+		LTail->NextPosition = PNew;
+		PNew->NextPosition = NULL;
+		LTail = PNew;
+	}
+
+	//free(PNew);
+	return L;
+}
+
+List SortList(List L)
+{
+	Position PHead;
+	Position PTail;
+	int HeadAddress;
+	int TailAddress;
+
+	HeadAddress = L->NextAddress;				//标记首节点的地址
+	PTail = L->NextPosition;
+	//找出尾节点
+	while (PTail != NULL)
+	{
+		if (PTail->NextAddress < 0)
+		{
+			TailAddress = PTail->Address;
+		}
+		PTail = PTail->NextPosition;
+	}
+
+	return L;
+}
+
+void TravalList(List L)
+{
+	Position P;
+	P = L->NextPosition;
+	while (P != NULL)
+	{
+		printf("%05d %d %05d\n", P->Address, P->Data, P->NextAddress);
+		P = P->NextPosition;
+	}
+	printf("\n");
 }
--- ReversingLinkedList/ReversingLinkedList.h
+++ ReversingLinkedList/ReversingLinkedList.h
@@ -2,6 +2,22 @@
 #define __REVERSINGLINKEDLIST_H
 
 #include <stdio.h>
+#include <malloc.h>
 
+typedef struct Node* PtrToNode;
+typedef PtrToNode	 List;
+typedef PtrToNode	 Position;
+
+List CreatLinkedList(int firstAddress, int length, List L);
+void TravalList(List L);
+List SortList(List L);
 
 #endif
+
+struct Node
+{
+	int Address;
+	int Data;
+	int NextAddress;
+	Position NextPosition;
+};
